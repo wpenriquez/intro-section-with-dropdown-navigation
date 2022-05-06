78f564690cf4e49afb8b5ad1bf9b524053fc4df0
wpenriquez <wilphredpaulenriquez@gmail.com>
2022-05-06 15:15:37 +0800

    further updated code for responsive view on medium and large devices

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -13,7 +13,7 @@ const Homepage = () => {
   // USE EFFECT FOR CHANGING HOMEPAGE IMAGE DEPENDING ON SCREEN SIZE
   useEffect(() => {
     const changeImage = () => {
-      if (window.innerWidth < 1023) {
+      if (window.innerWidth < 1024) {
         setViewImage(mobileImage);
       } else {
         setViewImage(desktopImage);
@@ -27,34 +27,34 @@ const Homepage = () => {
   return (
     <div>
       <main>
-        <div className="body-container flex flex-col xl:flex-row-reverse md:mt-[3%] xl:items-center xl:justify-evenly">
+        <div className="body-container flex flex-col lg:flex-row-reverse md:mt-[3%] lg:items-center lg:justify-evenly">
           {/* BODY IMAGE */}
-          <div className="body-image w-full 2xl:w-[30%] xl:w-[35%]">
+          <div className="body-image w-full lg:w-[35%] 2xl:w-[30%] xl:w-[35%]">
             <img
               src={viewImage}
               alt="homepage_img"
-              className="w-full md:h-[384px] md:object-cover md:object-top xl:h-auto"
+              className="w-full md:h-[384px] lg:h-auto md:object-cover md:object-top"
             />
           </div>
           {/* HOMEPAGE CONTENT */}
-          <div className="homepage-content text-center xl:text-left 2xl:flex flex-col justify-center px-5 mt-[6%] xl:px-0 xl:w-[35%] 2xl:w-[37%]">
+          <div className="homepage-content text-center lg:text-left flex flex-col justify-center px-5 mt-[6%] xl:px-0 lg:w-[40%] xl:w-[35%] 2xl:w-[37%]">
             {/* HOMEPAGE INTRO */}
-            <div className="homepage-intro">
-              <h1 className="text-[39px] xl:text-6xl 2xl:text-8xl font-bold text-black">
+            <div className="homepage-intro flex flex-col md:items-center lg:items-start">
+              <h1 className="text-[39px] md:text-6xl 2xl:text-8xl font-bold text-black">
                 Make remote work
               </h1>
-              <p className="mt-4 xl:mt-10 text-xl xl:w-[90%] 2xl:w-[65%]">
+              <p className="mt-4 xl:mt-10 text-xl md:text-2xl lg:text-xl 2xl:text-2xl md:w-[65%] lg:w-[100%] xl:w-[90%] 2xl:w-[68%]">
                 Get your team in sync, no matter your location. Streamline
                 processes, create team rituals, and watch productivity soar.
               </p>
             </div>
-            <div className="learn-more mt-[20%] xl:mt-[10%] xl:flex justify-start">
+            <div className="learn-more mt-[20%] md:mt-[10%] xl:flex justify-start">
               <a href="#" className="learn-more-btn">
                 Learn more
               </a>
             </div>
-            <div className="client-list mt-[25%] mb-5">
-              <ul className="flex items-center justify-center 2xl:justify-start">
+            <div className="client-list mt-[25%] md:mt-[15%] mb-5 md:mb-0 xl:mb-5">
+              <ul className="flex items-center justify-center xl:justify-start">
                 <li className="client-logo">
                   <img src={databiz} alt="databiz_logo" className="w-full" />
                 </li>
